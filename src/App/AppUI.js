5d4557132369bbeb2db0600f8e53423d3267a3b3
Juan Pablo Gonzales <juampi7237@gmail.com>
2022-10-10 21:05:11 -0400

    resolviendo pimier reto con otro loading

--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -9,6 +9,7 @@ import { TodoForm } from "../TodoForm";
 import { CreateTodoButton } from "../CreateTodoButton";
 import { MyLoader } from "../TodoLoader/TodoReactLoader";
 import { Modal } from "../Modal/idex";
+import { TodosLoading } from "../TodoLoader";
 
 function AppUI() {
   //Destructuramos los valores de nuestro conexto lo q esta guardado en el objeto value
@@ -29,7 +30,8 @@ function AppUI() {
 
       <TodoList>
         {error && <p>Desespérate, hubo un error...</p>}
-        {loading && <MyLoader />}
+        {/* {loading && <MyLoader />} */}
+        {loading && <TodosLoading />}
         {!loading && !searchedTodos.length && <p>¡Crea tu primer TODO!</p>}
 
         {searchedTodos.map((todo) => (
