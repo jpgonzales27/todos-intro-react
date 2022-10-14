4ab7368eca1949c7b76b5c741a54e192916c2839
Juan Pablo Gonzales <juampi7237@gmail.com>
2022-10-14 18:47:15 -0400

    instalando y usando react-icons

--- src/TodoItem/index.js
+++ src/TodoItem/index.js
@@ -1,24 +1,27 @@
-import React from 'react';
-import './TodoItem.css';
+import React from "react";
+import "./TodoItem.css";
+import { FaTrashAlt, FaCheckCircle } from "react-icons/fa";
 
 function TodoItem(props) {
   return (
     <li className="TodoItem">
       <span
-        className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}
+        className={`Icon Icon-check ${props.completed && "Icon-check--active"}`}
         onClick={props.onComplete}
       >
         √
       </span>
-      <p className={`TodoItem-p ${props.completed && 'TodoItem-p--complete'}`}>
+      <FaCheckCircle
+        className={`Icon Icon-check ${props.completed && "Icon-check--active"}`}
+        onClick={props.onComplete}
+      />
+      <p className={`TodoItem-p ${props.completed && "TodoItem-p--complete"}`}>
         {props.text}
       </p>
-      <span
+      <FaTrashAlt
         className="Icon Icon-delete"
         onClick={props.onDelete}
-      >
-        X
-      </span>
+      ></FaTrashAlt>
     </li>
   );
 }
